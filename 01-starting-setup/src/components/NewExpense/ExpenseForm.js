63cfa629f2f43c9efd57a88a9d2100729cfc1dc1
Ash Lai <z0975201508@gmail.com>
2021-06-25 11:04:51 +0800

    course 53. Wokring with Multiple States

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
     console.log(event.target.value);
+  };
+
+  const amountHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
 
   return (
@@ -14,12 +27,22 @@ const ExpenseForm = () => {
           <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label>Title</label>
-          <input type="number" min="0.01" step="0.01" />
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountHandler}
+          />
         </div>
         <div className="new-expense__control">
-          <label>Title</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">

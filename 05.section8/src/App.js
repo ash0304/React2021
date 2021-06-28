7ebbc7b7f9785e0da1bb2b9dd10e9da966ed474b
Ash Lai <z0975201508@gmail.com>
2021-06-28 14:46:32 +0800

    course 95. Managing a List Of Users via State

--- 05.section8/src/App.js
+++ 05.section8/src/App.js
@@ -4,11 +4,22 @@ import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
 function App() {
-  const [ users, setUsers ] = useState([]);
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+    console.log(usersList);
+  };
+
   return (
     <div>
-      <AddUser />
-      <UserList users={users} />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }

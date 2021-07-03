788b502b891e0e5caa22292dd1446eb416e591f5
Ash Lai <z0975201508@gmail.com>
2021-07-03 16:57:03 +0800

    course 146. Working on a More Complex Reducer Logic

--- 08.Section11/src/store/CartProvider.js
+++ 08.Section11/src/store/CartProvider.js
@@ -8,9 +8,32 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
+    // totalAmount 不變
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+    // 尋找已經存在重複item的index
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    // 已存在重複的item
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    // 若存在
+    if (existingCartItem) {
+      // 先淺拷貝現有item, 然後更新amount數值達到重複幾個只變化在amount
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      // 先拷貝現有state內的item(舊)
+      updatedItems = [...state.items];
+      // 更新指定位置item的所有值
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      // 若不存在，就正常的合併舊值與新值
+      updatedItems = state.items.concat(action.item);
+    }
+    
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,

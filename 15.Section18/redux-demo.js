3fca971cba8cc7efb13ca1afd850156dd4dba626
Ash Lai <z0975201508@gmail.com>
2021-07-05 14:27:29 +0800

    course 229. More Redux Basic

--- 15.Section18/redux-demo.js
+++ 15.Section18/redux-demo.js
@@ -2,9 +2,19 @@ const redux = require('redux');
 
 // It's a programming concept , have nothing to do with useReducer
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -20,3 +30,4 @@ store.subscribe(counterSubscriber);
 
 // dispatch the counterReducer
 store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });

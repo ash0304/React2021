77fd1c2ca4dd86a799629f54d81d34abfef2ac7f
Ash Lai <z0975201508@gmail.com>
2021-07-12 06:12:19 +0800

    course 181. Sending a POST Request

--- 11.Section14/src/App.js
+++ 11.Section14/src/App.js
@@ -14,22 +14,27 @@ function App() {
     setError(null);
     try {
       // Promise 改 async await 寫法 -> just syntax sugar
-      const response = await fetch('https://react-http-99265-default-rtdb.firebaseio.com/movies.json');
+      const response = await fetch(
+        'https://react-http-99265-default-rtdb.firebaseio.com/movies.json'
+      );
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      // 資料處理
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -41,9 +46,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      'https://react-http-99265-default-rtdb.firebaseio.com/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
 
   let content = <p>Found no movies.</p>;
 

fde3de96ff1a846b35bae57d09e6999620aae7eb
Ash Lai <z0975201508@gmail.com>
2021-07-12 05:56:11 +0800

    course 180. Preparing The Project For The Next Steps

--- 11.Section14/src/App.js
+++ 11.Section14/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -13,10 +14,11 @@ function App() {
     setError(null);
     try {
       // Promise 改 async await 寫法 -> just syntax sugar
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-99265-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
-        throw new Error('Somthing went wrong!');
+        throw new Error('Something went wrong!');
       }
+
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -39,6 +41,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
@@ -55,6 +61,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>

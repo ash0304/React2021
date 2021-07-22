5939a605e5a706fced5fa36266218ab78bbfe979
Ash Lai <z0975201508@gmail.com>
2021-07-22 10:52:55 +0800

    course 255. Using useEffect with Redux

--- 16.Section19/src/App.js
+++ 16.Section19/src/App.js
@@ -1,14 +1,24 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-99265-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
-      {cartIsVisible && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
--- 16.Section19/src/store/cart-slice.js
+++ 16.Section19/src/store/cart-slice.js
@@ -2,8 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: { items: [], totalQuantity: 0 },
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -14,7 +21,7 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          title: newItem.title,
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
@@ -29,7 +36,6 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },

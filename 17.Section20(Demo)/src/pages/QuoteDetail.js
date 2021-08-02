242fbb0c1d542dc372f776dee4ddbeb3ef150dc8
Ash Lai <z0975201508@gmail.com>
2021-08-02 16:00:15 +0800

    course 285. Getting Creative With Nested Routes

--- 17.Section20(Demo)/src/pages/QuoteDetail.js
+++ 17.Section20(Demo)/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Route } from 'react-router';
+import { useParams, Route, Link } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
@@ -15,13 +15,19 @@ const QuoteDetail = () => {
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No quote found!</p>
+    return <p>No quote found!</p>;
   }
 
   return (
     <React.Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
